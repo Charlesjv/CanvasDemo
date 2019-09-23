8d76dd1ccc524018a1c9aed4621f394d6df60fc2
macstudent <charles764jaison@gmail.com>
2019-09-23 11:23:48 -0400

    Adding Background and drawing a line

--- app/src/main/java/com/example/canvasdemo/MainActivity.java
+++ app/src/main/java/com/example/canvasdemo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.canvasdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1.Setup the frame
+        ImageView ivframe = (ImageView) findViewById(R.id.imageView);
+        Bitmap b = Bitmap.createBitmap(300,500,Bitmap.Config.ARGB_8888);
+        //2. setup the canvas
+        Canvas canvas = new Canvas(b);
+        //3. setup your paintbrush
+        Paint paintbrush = new Paint();
+
+        //1. set the background
+
+            canvas.drawColor(Color.GREEN);
+
+         //2. draw the line
+
+            canvas.drawLine(10,50,200,50,paintbrush);
+
+
+
+        ivframe.setImageBitmap(b);
     }
 }
